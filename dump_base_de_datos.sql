eada5a330cf962f71fdeed9e913211a2f4a534b4
cristian bono <cristianbono87@gmail.com>
2020-05-04 21:21:25 -0300

    se crean tablas en lechuapp

---
+++ dump_base_de_datos.sql
@@ -0,0 +1,8 @@
+drop database lechuapp;
+create database lechuapp;
+use lechuapp;
+create table client;
+CREATE TABLE client_detail;
+CREATE TABLE order;
+CREATE TABLE product;
+CREATE TABLE category;
