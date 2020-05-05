0c6eeb8d59090e9e4798baa0e146dfa6aac73fdc
Matias Villafañe <mattiasgab@hotmail.com>
2020-05-04 21:45:08 -0300

    Establecer conexion con base de datos

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const database = require('./dbconnect');
 
 const app = express();
 
 const port = 3000;
 
-app.listen(port, function() {
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(port, function () {
     console.log(`Escuchando en el puerto ${port}`);
 });
