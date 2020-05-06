c5ab889f2ff63f76cb3ab2d6f443c5d7365263d9
Ivan <ivanenoriega@gmail.com>
2020-05-06 17:50:30 -0300

    revertd last changes

--- server.js
+++ server.js
@@ -3,16 +3,11 @@ const bodyParser = require("body-parser");
 const database = require("./dbconnect");
 
 const app = express();
-
-const PORT = process.env.PORT || 3000;
+const port = 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use("/", (req, res) => {
-  res.send("Hello from lechuapp");
-});
-
-app.listen(PORT, function () {
-  console.log(`Escuchando en el puerto ${PORT}`);
+app.listen(port, function () {
+  console.log(`Escuchando en el puerto ${port}`);
 });
