7b3a992c14f2c7817ee1ca9bad5b452b72b33f70
Marina <marinafaenze@hotmail.com>
2020-05-06 20:51:50 -0300

    se creo la funcion crearClientes en controladorClientes

--- controladorClientes.js
+++ controladorClientes.js
@@ -10,11 +10,24 @@ function obtenerClientes(req, res){
 }
 
 function obtenerClientesPorId(req, res) {    
-    let id = req.query.id;
+    let id = req.params.id;
     const sql = "SELECT * FROM client JOIN client_detail ON idCliente = client.id WHERE client.id = " + id;
     conexion.query(sql, function(err, result){
         if(err) return console.log("Se ha producido un error en la consulta cliente id", err);        
         res.send(result);
+    });    
+}
+
+function crearNuevoCliente(req, res){
+
+    let body = req.body;
+    let nombre = body.nombre;
+    let telefono = body.telefono;    
+
+    conexion.query("INSERT INTO client (nombre, telefono) VALUES (?,?)", [nombre, telefono], 
+    function(err, result){
+        if(err) return console.log("Hubo un error en el insert de cliente", err.message);        
+        res.send("ID del nuevo cliente", result);
     });
     
 }
@@ -25,8 +38,6 @@ module.exports = {
 };
 
 /*
-
-Obtener un cliente especifico (GET "/client/id_cliente"). Traer los datos de la tabla client y la tabla client_detail.
 Crear un nuevo cliente (POST "/client"). Validar que todos los datos obligatorios (tabla client) estén presentes. Devolver el ID del nuevo client en la respuesta.
 Editar un cliente existente (PUT "/client").
 Añada un elemento
