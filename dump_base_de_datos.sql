5aa9abac911574321dd50e2564010471e5148d03
unknown <mansillaign.90@gmail.com>
2020-05-06 19:53:43 -0300

    asdasdmasd

---
+++ dump_base_de_datos.sql
@@ -0,0 +1,9 @@
+DROP DATABASE lechuapp;
+CREATE DATABASE lechuapp;
+USE lechuapp;
+CREATE TABLE client;
+CREATE TABLE client_detail;
+CREATE TABLE order;
+CREATE TABLE product;
+CREATE TABLE category;
+
