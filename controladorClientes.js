0ae162fcd0b90f5e61d1bc0e0efca4ffeca3c062
Marina <marinafaenze@hotmail.com>
2020-05-06 21:19:17 -0300

    se exporto funcion crear clientes

--- controladorClientes.js
+++ controladorClientes.js
@@ -4,37 +4,39 @@ function obtenerClientes(req, res){
 
     const sql = "SELECT * FROM client JOIN client_detail ON idCliente = client.id ORDER BY client.id DESC";
     conexion.query(sql, function(err, result){
-        if(err) return console.log("Se ha producido un error en la consulta cliente", err);        
+        if(err) return res.status(404).send("Hubo un error en la consulta cliente");        
         res.json(result);
     });
 }
 
 function obtenerClientesPorId(req, res) {    
     let id = req.params.id;
+    
     const sql = "SELECT * FROM client JOIN client_detail ON idCliente = client.id WHERE client.id = " + id;
+    
     conexion.query(sql, function(err, result){
-        if(err) return console.log("Se ha producido un error en la consulta cliente id", err);        
-        res.send(result);
+        if(err) return console.log("Se ha producido un error en la consulta cliente id", err);  
+        res.json(result);
     });    
 }
 
 function crearNuevoCliente(req, res){
-
     let body = req.body;
     let nombre = body.nombre;
     let telefono = body.telefono;    
 
     conexion.query("INSERT INTO client (nombre, telefono) VALUES (?,?)", [nombre, telefono], 
     function(err, result){
-        if(err) return console.log("Hubo un error en el insert de cliente", err.message);        
-        res.send("ID del nuevo cliente", result);
+        if(err) return console.log("Hubo un error en el insert de cliente", err.message);         
+        res.send(result);
     });
     
 }
 
 module.exports = { 
     obtenerClientes : obtenerClientes,
-    obtenerClientesPorId : obtenerClientesPorId
+    obtenerClientesPorId : obtenerClientesPorId,
+    crearNuevoCliente : crearNuevoCliente
 };
 
 /*
