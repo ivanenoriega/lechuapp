24fad098e9536e67e5b4413934731e5509a68835
matias-villafane <54558338+matias-villafane@users.noreply.github.com>
2020-05-11 20:24:56 -0300

    Server Error handler

--- server.js
+++ server.js
@@ -9,12 +9,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/ping", (req, res) => res.send("Lechu pong!"));
+app.get("/error", function () { console.log("error"); throw new Error() });
 
 app.listen(port, function () {
   console.log(`Escuchando en el puerto ${port}`);
 });
 
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   console.error(err.stack);
-  res.status(500).send('Something broke!');
+  res.status(500).send('Se rompio todo!');
 });
