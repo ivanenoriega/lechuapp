0e72eb536c5a6dbf471132d6ee2a298d7f975822
Matias Villafañe <mattiasgab@hotmail.com>
2020-05-11 21:55:42 -0300

    status code client - GET

--- controladorClientes.js
+++ controladorClientes.js
@@ -5,7 +5,10 @@ function obtenerClientes(req, res) {
     const sql = "SELECT * FROM client JOIN client_detail ON idCliente = client.id ORDER BY client.id DESC";
     conexion.query(sql, function (err, result) {
         if (err) return res.status(404).send("Hubo un error en la consulta cliente");
-        res.json(result);
+        if (result.length === 0) {
+            return res.status(204).send(result);
+        }
+        return res.json(result);
     });
 }
 
@@ -13,14 +16,18 @@ function obtenerClientesPorId(req, res) {
     let id = req.params.id;
 
     const sql = "SELECT * FROM client JOIN client_detail ON idCliente = client.id WHERE client.id = " + id;
-
-    conexion.query(sql, function (err, result) {
-        console.log(JSON.stringify(res.body));
-        if (err) return console.log("Se ha producido un error en la consulta cliente id", err);
-        res.json(result);
-
-
-    });
+    if (isNaN(id)) {
+        res.status(400).send({ message: "id param invalido" });
+    } else {
+        conexion.query(sql, function (err, result) {
+            console.log(JSON.stringify(result));
+            if (err) return console.log("Se ha producido un error en la consulta cliente id", err);
+            if (result.length === 0) {
+                return res.status(404).send({ message: "usuario no encontrado" });
+            }
+            return res.json(result);
+        });
+    }
 }
 
 function crearNuevoCliente(req, res) {
