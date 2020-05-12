77fe3f795115a89f3a9c5fccfa7311c5d5c3ebd3
matias-villafane <54558338+matias-villafane@users.noreply.github.com>
2020-05-11 21:23:29 -0300

    controlador clientes

--- controladorClientes.js
+++ controladorClientes.js
@@ -1,41 +1,44 @@
 const conexion = require('./dbconnect');
 
-function obtenerClientes(req, res){
+function obtenerClientes(req, res) {
 
     const sql = "SELECT * FROM client JOIN client_detail ON idCliente = client.id ORDER BY client.id DESC";
-    conexion.query(sql, function(err, result){
-        if(err) return res.status(404).send("Hubo un error en la consulta cliente");        
+    conexion.query(sql, function (err, result) {
+        if (err) return res.status(404).send("Hubo un error en la consulta cliente");
         res.json(result);
     });
 }
 
-function obtenerClientesPorId(req, res) {    
+function obtenerClientesPorId(req, res) {
     let id = req.params.id;
-    
+
     const sql = "SELECT * FROM client JOIN client_detail ON idCliente = client.id WHERE client.id = " + id;
-    
-    conexion.query(sql, function(err, result){
-        if(err) return console.log("Se ha producido un error en la consulta cliente id", err);  
+
+    conexion.query(sql, function (err, result) {
+        console.log(JSON.stringify(res.body));
+        if (err) return console.log("Se ha producido un error en la consulta cliente id", err);
         res.json(result);
-    });    
+
+
+    });
 }
 
-function crearNuevoCliente(req, res){
+function crearNuevoCliente(req, res) {
     let body = req.body;
     let nombre = body.nombre;
-    let telefono = body.telefono;    
+    let telefono = body.telefono;
 
-    conexion.query("INSERT INTO client (nombre, telefono) VALUES (?,?)", [nombre, telefono], 
-    function(err, result){
-        if(err) return console.log("Hubo un error en el insert de cliente", err.message);         
-        res.send(result);
-    });
-    
+    conexion.query("INSERT INTO client (nombre, telefono) VALUES (?,?)", [nombre, telefono],
+        function (err, result) {
+            if (err) return console.log("Hubo un error en el insert de cliente", err.message);
+            res.send(result);
+        });
+
 }
 
-module.exports = { 
-    obtenerClientes : obtenerClientes,
-    obtenerClientesPorId : obtenerClientesPorId,
-    crearNuevoCliente : crearNuevoCliente
+module.exports = {
+    obtenerClientes: obtenerClientes,
+    obtenerClientesPorId: obtenerClientesPorId,
+    crearNuevoCliente: crearNuevoCliente
 };
 
