cd76d35600e38b7221874ca1328caf032422a9ec
ricardo <ricardogenaizir1@gmail.com>
2020-05-13 20:55:51 -0300

    creado end points de get para  orders(prucharse)

---
+++ controladores/controladorOrders.js
@@ -0,0 +1,47 @@
+const conexion = require('../dbconnect');
+
+function listarOrdenes(req,res){
+
+// nom client nom producto pre unit cantidad y  total  de la orden. /select / join / group by.
+ var id = req.params.id;
+
+ var sql = 'select pu.id,c.nombre , p.nombre, p.precio, pu.cantidad, sum(p.precio * pu.cantidad)'+
+    'from purchase pu join client c on pu.client_id=c.id join product p on pu.product_id=p.id ';
+
+ if(id){
+    sql = sql + 'where pu.id= '+id +' group by pu.id ';
+
+    conexion.query(sql,function(error,respuesta){
+        if(error){
+            return res.status(404).send("Hubo un error en la consulta para traer ordenes por id");  
+        }
+        if(respuesta==0){
+            return res.status(404).send("Hubo un error no hay una orden de compra con ese id");
+        }
+
+        res.json(respuesta);
+    });
+ }
+
+  
+ if(!id){
+        sql = sql + ' group by pu.id';
+
+    conexion.query(sql,function(error,respuesta){
+        if(error){
+            return res.status(404).send("Hubo un error en la consulta para traer ordenes por id");  
+        }
+        if(respuesta==0){
+            return res.status(404).send("Hubo un error no hay una orden de compra con ese id");
+        }
+
+        res.json(respuesta);
+    });
+ }
+
+
+}
+
+module.exports ={
+    listarOrdenes:listarOrdenes
+}
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const database = require("./dbconnect");
 const commons = require("./server/commons");
 const controladorClient = require('./controladores/controladorClientes');
+const controladorOrders = require('./controladores/controladorOrders');
 // comentario para arreglar merge
 const app = express();
 const port = process.env.PORT || 3000;
@@ -15,25 +16,8 @@ app.get("/error", function () { console.log("error"); throw new Error() });
 app.get("/client", controladorClient.obtenerClientes);
 app.get("/client/:id", controladorClient.obtenerClientesPorId);
 app.post("/client", controladorClient.crearNuevoCliente);
-app.get("/orders",)
-
-
-/*
-
-Obtener todos las ordenes (GET "/orders"). Debe figurar el nombre del cliente, 
-el nombre del producto comprado, el precio unitario, la cantidad que compró, y
- el total de la orden. /select / join / group by.
-(GET "/orders") Modificar la query anterior para que el endpoint pueda recibir
- un parametro tipo QUERY PARAM. Verificar que exista el query param, y en el caso de que si,
-  agregar el WHERE a la consulta. Debe figurar el nombre del cliente, el nombre del producto comprado,
- el precio unitario, la cantidad que compro, y el total de la orden. /select / join / where / group by.
-
-
-Crear PR (pull request solicitando los cambios)
-Adjuntar PR en esta card
-Añada un elemento
-
-*/
+app.get("/orders",controladorOrders.listarOrdenes);
+app.get("/orders/:id",controladorOrders.listarOrdenes);
 
 app.listen(port, function () {
   console.log(`Escuchando en el puerto ${port}`);
