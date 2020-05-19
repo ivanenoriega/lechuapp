6b034207e18f19067a3fb3ccdea5784aa74d4556
Matias Villafañe <mattiasgab@hotmail.com>
2020-05-18 22:22:09 -0300

    Fix varios

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const database = require("./dbconnect");
+//const database = require("./dbconnect");
 const commons = require("./server/commons");
 const controladorClient = require('./controladores/controladorClientes');
 const controladorOrders = require('./controladores/controladorOrders');
+const controladorProductos = require('./controladores/contoladorProductos');
 // comentario para arreglar merge
 const app = express();
 const port = process.env.PORT || 3000;
@@ -18,6 +19,10 @@ app.get("/client/:id", controladorClient.obtenerClientesPorId);
 app.post("/client", controladorClient.crearNuevoCliente);
 app.get("/orders",controladorOrders.listarOrdenes);
 app.get("/orders/:id",controladorOrders.listarOrdenes);
+
+app.get('/products/:id',controladorProductos.obtenerProductosPorId);
+app.get('/products',controladorProductos.obtenerProductos);
+app.post('/products',controladorProductos.crearNuevoProducto);
 
 app.listen(port, function () {
   console.log(`Escuchando en el puerto ${port}`);
